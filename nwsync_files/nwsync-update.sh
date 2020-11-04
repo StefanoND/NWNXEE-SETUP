4c26f15e41573d37a7b0798368e21bbfe53e1989
Ecsyend <45202623+Ecsyend@users.noreply.github.com>
2020-11-04 11:07:00 +0000

    Update nwsync-update.sh

--- nwsync_files/nwsync-update.sh
+++ nwsync_files/nwsync-update.sh
@@ -4,4 +4,5 @@ export PATH=~/nwsync
 
 pushd ~/nwsync
 
-./nwsync_gui
+# Example ./nwsync_write --description="Download" /home/ecsyend/www/nwsync/nwsyncdata /home/ecsyend/nwn/modules/module.mod
+./nwsync_write --description="Module's Required Files" /home/USER/www/nwsync/nwsyncdata /path/to/modules/module.mod
