c5623f754457785b2c3ec7eba02e62c58dfbd8c1
Stefano Dobbin <45202623+Ecsyend@users.noreply.github.com>
2020-12-04 17:32:06 +0000

    Update nwsync-update.sh

--- nwsync_files/nwsync-update.sh
+++ nwsync_files/nwsync-update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export PATH=~/nwsync
+export PATH=/home/USER/nwsync
 
-pushd ~/nwsync
+pushd /home/USER/nwsync
 
 # Example ./nwsync_write --description="Download" /home/ecsyend/www/nwsync/nwsyncdata /home/ecsyend/nwn/modules/module.mod
-./nwsync_write --description="Module's Required Files" /home/USER/www/nwsync/nwsyncdata /path/to/modules/module.mod
+./nwsync_write --description="Module's Required Files" /home/USER/www/nwsync/nwsyncdata ~/nwn/userdir/modules/module.mod
