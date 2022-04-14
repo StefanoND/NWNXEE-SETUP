a01ba3a20559de42ba785d35e7bb4def3f80aba1
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2022-04-14 11:56:55 +0100

    Update misc.sh

--- nwnx_files/misc.sh
+++ nwnx_files/misc.sh
@@ -1,4 +1,8 @@
+sudo curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
+
 sudo apt install redis npm -y
+sudo npm install -g npm@8.6.0
 
 // In VSCode
 npm install ioredis dotenv discord.js --save
