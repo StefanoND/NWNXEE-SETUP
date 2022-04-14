32b7d845452ff8028bffc2527c7540d1406ad42e
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2022-04-14 09:31:05 +0100

    Update misc.sh

--- nwnx_files/misc.sh
+++ nwnx_files/misc.sh
@@ -2,6 +2,4 @@ sudo apt install redis
 sudo apt install npm
 
 // In VSCode
-npm install ioredis dotenv discord.js discord.io winston --save
-
-npm install https://github.com/woor/discord.io/tarball/gateway_v6
+npm install ioredis dotenv discord.js --save
