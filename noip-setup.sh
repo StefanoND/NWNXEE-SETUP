1e42b24da6ba1381d9bfcd2e0bdc169eec01bcf0
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-03-29 10:54:32 +0100

    Update noip-setup.sh

--- noip-setup.sh
+++ noip-setup.sh
@@ -1,17 +1,17 @@
 # Install pre-requisite (if it isn't already)
-apt install make gcc -y
+sudo apt install make gcc -y
 #
 # Change folder
-cd /usr/local/src/
+cd /usr/local/src
 #
 # Download NO-IP
 sudo wget http://www.noip.com/client/linux/noip-duc-linux.tar.gz
 #
 # Unzip it
-tar xf noip-duc-linux.tar.gz
+sudo tar xf noip-duc-linux.tar.gz
 #
 # Change folder
-cd noip-2.1.9-1/
+cd noip-2.1.9-1
 #
 # Install and configure with your no-ip2 credentials, leave everything at default
 sudo make install
@@ -22,31 +22,17 @@ sudo /usr/local/bin/noip2 -C
 #
 # Make it run on boot (You can use other app instead of Nano if you wish)
 # Create this file and copy-paste the following
-sudo nano /etc/systemd/system/noip2.service
-#
-#-----------------------------------------------------------------------------
-#                                  NOTE
-#-----------------------------------------------------------------------------
-# [Unit]
-# Description=noip2 service
-#
-# [Service]
-# Type=forking
-# ExecStart=/usr/local/bin/noip2
-# Restart=always
-#
-# [Install]
-# WantedBy=default.target
-#-----------------------------------------------------------------------------
+sudo touch /etc/systemd/system/noip2.service
+curl https://raw.githubusercontent.com/StefanoND/NWNXEE-SETUP/main/noip_files/noip2.service -o - | sudo tee /etc/systemd/system/noip2.service
 #
 # Reload Systemctl
 sudo systemctl daemon-reload
 #
 # Enable NOIP2 service
-sudo systemctl enable noip2
+sudo systemctl enable --now noip2
 #
 # Reboot your PC
-shutdown -r now
+sudo reboot now
 #
 # Now you can check if it's working or not, if it is congratulations!
 systemctl status noip2
