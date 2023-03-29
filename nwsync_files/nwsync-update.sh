61340c4433bc654133dfa4985d08e50c7c6b94f8
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-03-29 21:27:04 +0100

    Update nwsync-update.sh

--- nwsync_files/nwsync-update.sh
+++ nwsync_files/nwsync-update.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ $EUID -ne 0 ]
+    then
+        echo
+        echo "This script must run as root to function."
+        echo
+        sleep 1s
+        exit 1
+fi
+
 # Change USER to your username
 export PATH=/home/USER
 
@@ -7,3 +16,5 @@ pushd $PATH/nwsync
 
 # Example ./nwsync_write --description="Download" $PATH/www/nwsync/nwsyncdata $PATH/nwn/modules/module.mod
 ./nwsync_write --description="Module's Required Files" $PATH/www/nwsync/nwsyncdata $PATH/nwn/userdir/modules/module.mod
+
+exit 0
